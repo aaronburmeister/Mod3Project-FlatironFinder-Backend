1c86a7cdbd532ecb6bdf537e0f7337ae6cf41561
aaronburmeister <f34r91@gmail.com>
2020-03-09 11:38:02 -0600

    changed error message if username was wrong

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,10 +4,10 @@ class AuthenticationController < ApplicationController
         @user = User.find_by(username: params[:username])
 
         if !@user
-            render json: { message: "Wrong Username" }, status: :unauthorized
+            render json: { message: "Username Not Found" }, status: :unauthorized
         else
             if !@user.authenticate(params[:password])
-                render json: { message: "Wrong Password" }, status: :unauthorized
+                render json: { message: "Incorrect Password" }, status: :unauthorized
             else
                 user = { id: @user.id, name: @user.username}
                 secret = Rails.application.secrets.secret_key_base
