65faf77a41866802324f8f2f5a4b98e5e05e0762
aaronburmeister <f34r91@gmail.com>
2020-03-10 10:05:08 -0600

    added validations for email and campus

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true, length: { minimum: 8 }
+    validates :email, presence: true
+    validates :campus, presence: true
 end
